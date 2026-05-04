1c77e26e0d58620011595878a4b774b7cbb4a22f
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-05-04 15:04:50 +0200

    Section 5: Presentation layer: Flux: Returning multiple objects in HTTP Response Body

--- src/main/java/net/javaguides/reactive/ws/users/presentation/UserController.java
+++ src/main/java/net/javaguides/reactive/ws/users/presentation/UserController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -70,4 +70,28 @@ public class UserController {
         return Mono.just(user);
     }
 
+    @GetMapping
+    public Flux<UserRest> getUsers(@RequestParam(value = "offset", defaultValue = "0") final Integer offset,
+                                   @RequestParam(value = "limit", defaultValue = "50") final Integer limit) {
+        log.info("🌐 - Get Users Request called");
+
+        // Simulate fetching all users logic (e.g., retrieving from database)
+        // For demonstration, we will just log the request
+        return Flux.just(
+                        UserRest.builder()
+                                .id(UUID.randomUUID())
+                                .firstName("John")
+                                .lastName("Doe")
+                                .email("john@gmail")
+                                .build(),
+                        UserRest.builder()
+                                .id(UUID.randomUUID())
+                                .firstName("Jane")
+                                .lastName("Smith")
+                                .email("jane@gmail")
+                                .build()
+                ).doOnNext(user -> log.info("📋 - User: {}", user))
+                .doOnComplete(() -> log.info("✅✅ - All users fetched successfully"))
+                .doOnError(error -> log.error("❌ - Error fetching users: {}", error.getMessage()));
+    }
 }
