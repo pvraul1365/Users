405f8d2a0224aea8438a3a6fd575fae61eb28674
Raúl Pérez Vicente <rperezv@ayesa.com>
2026-05-08 08:58:48 +0200

    Section 14: Validating JSON Web Token (JWT) 83. Adding JWT Authentication Filter to the Spring Security filter chain

--- src/main/java/net/javaguides/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/net/javaguides/reactive/ws/users/infrastructure/WebSecurity.java
@@ -1,15 +1,18 @@
 package net.javaguides.reactive.ws.users.infrastructure;
 
+import net.javaguides.reactive.ws.users.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 /**
  * WebSecurity
@@ -26,7 +29,10 @@ public class WebSecurity {
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-                                                     ReactiveAuthenticationManager authenticationManager) {
+                                                     ReactiveAuthenticationManager authenticationManager,
+                                                     JwtService jwtService) {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService);
+
         http
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
@@ -35,7 +41,9 @@ public class WebSecurity {
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .authenticationManager(authenticationManager);
+                .authenticationManager(authenticationManager)
+                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 
         return http.build();
     }
